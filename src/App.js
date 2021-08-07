885fa5ba623b2abb7104bedb1f44d81eae4706f0
dpgehani20 <69641659+dpgehani20@users.noreply.github.com>
2021-08-07 20:02:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./styles.css";
+import styled from "styled-components";
+import Axios from "axios";
+import City from "./components/City";
+import Weather from "./components/Weather";
+
+export const WeatherIcons = {
+  "01d": "/images/sunny.svg",
+  "01n": "/images/night.svg",
+  "02d": "/images/day.svg",
+  "02n": "/images/cloudy-night.svg",
+  "03d": "/images/cloudy.svg",
+  "03n": "/images/cloudy.svg",
+  "04d": "/images/perfect-day.svg",
+  "04n": "/images/cloudy-night.svg",
+  "09d": "/images/rain.svg",
+  "09n": "/images/rain-night.svg",
+  "10d": "/images/rain.svg",
+  "10n": "/images/rain-night.svg",
+  "11d": "/images/storm.svg",
+  "11n": "/images/storm.svg"
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 380px;
+  padding: 20px 10px;
+  margin: auto;
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 0 #555;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span`
+  color: black;
+  margin: 20px auto;
+  font-size: 18px;
+  font-weight: bold;
+`;
+// const CloseButton = styled.span`
+//   padding: 2px 3px;
+//   background-color: black;
+//   border-radius: 50%;
+//   color: white;
+//   position: absolute;
+// `;
+
+function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`
+    );
+    updateWeather(response.data);
+  };
+  return (
+    <Container>
+      <AppLabel>React Weather App</AppLabel>
+      {city && weather ? (
+        <Weather weather={weather} city={city} />
+      ) : (
+        <City updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </Container>
+  );
+}
+
+export default App;
